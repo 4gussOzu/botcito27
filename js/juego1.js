379f31e1a8d213bdacdec906d7c53c61cdd13488
4gusOzu <marianoagustinozuna@gmail.com>
2023-08-12 19:03:42 -0300

    botcito27-v1.0.5

--- js/juego1.js
+++ js/juego1.js
@@ -1,83 +1,140 @@
-const Piedra = 0;
-const Papel = 1;
-const Tijera = 2;
+const seccionBatalla = document.getElementById('campo-batalla');
+const imgAtaqueJugador = document.getElementById('img-ataque-jugador');
+const imgAtaquePc = document.getElementById('img-ataque-pc');
+const btnPiedra = document.getElementById('btn-piedra');
+const btnPapel = document.getElementById('btn-papel');
+const btnTijeras = document.getElementById('btn-tijeras');
 
-const EMPATE = 0;
-const WIN = 1;
-const LOSE = 2;
+let opcionJugador;
+let opcionPc;
+let imgJugador;
+let imgPc;
 
-const piedraBtn = document.getElementById("piedra");
-const papelBtn = document.getElementById("papel");
-const tijeraBtn = document.getElementById("tijera");
-const AnuncioResultados = document.getElementById("texto-importante");
-const jugadorImg = document.getElementById("jugadorImg");
-const botImg = document.getElementById("botImg");
-
-piedraBtn.addEventListener("click", ()=>{
-    console.log("piedra");
-    juego(Piedra);
-});
-papelBtn.addEventListener("click", ()=>{
-    console.log("papel");
-    juego(Papel);
-});
-tijeraBtn.addEventListener("click", ()=>{
-    console.log("tijera");
-    juego(Tijera);
-});
-
-function juego(EleccionJugador){
-    const randomisador = Math.floor(Math.random() * 3);
-    console.log(randomisador);
-    const resultados = calculadora(EleccionJugador, randomisador);
-
-    switch(resultados){
-        case EMPATE:
-            AnuncioResultados.innerHTML = "EMPATEEEE!!!";
-            break;
-        case WIN:
-            AnuncioResultados.innerHTML = "GANASTEEE!!!";
-            break;
-        case LOSE:
-            AnuncioResultados.innerHTML = "PERDSITE >:D"
-            break;
+const imagenes = [
+    {
+        name: "Piedra",
+        url: "img/Piedra.PNG" 
+    },
+    {
+        name: "Papel",
+        url: "img/Papel.PNG" 
+    },
+    {
+        name: "Tijeras",
+        url: "img/Tijeras.PNG" 
     }
+];
+
+
+
+function iniciar(){
+    seccionBatalla.style.display = 'none';
+};
+
+btnPiedra.addEventListener('click', function(){
+    opcionJugador = "Piedra";
+    opPc();
+});
+
+btnPapel.addEventListener('click', function(){
+    opcionJugador = "Papel";
+    opPc();
+});
+
+btnTijeras.addEventListener('click', function(){
+    opcionJugador = "Tijeras";
+    opPc();
+})
+
+
+function opPc(){
+    var aleaorio = nAleatorio();
+
+    if(aleaorio == 0){
+        opcionPc = "Piedra";
+    }else if(aleaorio == 1){
+        opcionPc = "Papel";
+    }else if(aleaorio == 2){
+        opcionPc = "Tijeras"
+    };
+
+    batalla();
+
+};
+
+function batalla(){
+    if(opcionJugador == opcionPc){
+        swal({
+            title: "Empate",
+            text: "toca el boton para seguir jugando!",
+            icon: "info",
+            button: "retry!",
+          });
+    }else if(opcionJugador == "Piedra" && opcionPc == "Tijeras"){
+     
+        swal({
+            title: "Ganaste!",
+            text: "toca el boton para seguir jugando!",
+            icon: "success",
+            button: "retry!",
+          });
+    }else if(opcionJugador == "Papel" && opcionPc == "Piedra"){
+    
+       swal({
+        title: "Ganaste!",
+        text: "toca el boton para seguir jugando!",
+        icon: "success",
+        button: "retry!",
+      });
+    }else if(opcionJugador == "Tijeras" && opcionPc == "Papel"){
+     
+       swal({
+        title: "Ganaste!",
+        text: "toca el boton para seguir jugando!",
+        icon: "success",
+        button: "retry!",
+      });
+    }else{
+    
+        swal({
+            title: "Perdiste",
+            text: "toca el boton para seguir jugando!",
+            icon: "error",
+            button: "retry!",
+          });
+    };
+
+    addImagenes();
+
 }
 
-function calculadora(EleccionJugador, randomisador){
-    if(EleccionJugador == randomisador){
-        console.log("empate");
-        return EMPATE;
+
+function nAleatorio(){
+    let n = Math.floor(Math.random() * 3);
+    return n;
+}
+
+
+function addImagenes(){
+    for(let i=0;i<imagenes.length;i++){
+        if(opcionJugador == imagenes[i].name){
+            imgJugador = imagenes[i].url;
+            var inserta = `<img class="img-batalla" src=${imgJugador} alt="">`;
+            imgAtaqueJugador.innerHTML = inserta;
+        };
         
-    }else if(EleccionJugador === Piedra){
+        if(opcionPc == imagenes[i].name){
+            imgPc = imagenes[i].url;
+            var inserta = `<img class="img-batalla" src=${imgPc} alt="">`;
+            imgAtaquePc.innerHTML = inserta;
+        };
+        
+    };
 
-        if(randomisador === Papel){    
-            console.log("perdiste");
-            return LOSE;
-        }
-        if(randomisador === Tijera){
-            console.log("ganaste");
-            return WIN;
-        }
-    }else if(EleccionJugador === Papel){
 
-        if(randomisador=== Tijera){
-            console.log("perdiste");
-            return LOSE;
-        }
-        if(randomisador === Piedra){
-            console.log("ganaste");
-            return WIN;
-        }        
-    }else if(EleccionJugador === Tijera){
+    seccionBatalla.style.display = 'flex';
+    
+};
 
-        if(randomisador === Piedra){
-            console.log("perdiste");
-            return LOSE;
-        }
-        if(randomisador === Papel){
-            console.log("ganaste");
-            return WIN;
-        }
-    }
-}
+
+window.addEventListener('load', iniciar);
